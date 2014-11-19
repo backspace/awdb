b4b37f1a16b04e2c5522a3280dbb83c95dbb80b6
Buck Doyle <b@chromatin.ca>
2014-11-19 12:52:47 -0500

    Hack issue route to load features

--- app/routes/issue.js
+++ app/routes/issue.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('issue', params.issue_id);
+    // FIXME why this one weird hack?
+    var issue = this.store.find('issue', params.issue_id);
+
+    var features = this.store.find('feature');
+
+    return features.then(function() {
+      return issue;
+    });
   }
 });
