9f9b44f333373a4e45045fa353ecded7fa1d60b1
Buck Doyle <b@chromatin.ca>
2014-11-21 21:21:09 -0500

    Add rudimentary issue creation

--- app/routes/issues.js
+++ app/routes/issues.js
@@ -3,5 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('issue');
+  },
+
+  actions: {
+    edit: function() {
+      this.controllerFor('issue').set('isEditing', true);
+    },
+
+    doneEditing: function() {
+      this.controllerFor('issue').set('isEditing', false);
+      this.modelFor('issue').save();
+    },
+
+    createIssue: function() {
+      this.send('edit');
+      var newIssue = this.get('store').createRecord('issue');
+      this.transitionTo('issue', newIssue.save());
+    }
   }
 });
---
+++ tests/acceptance/create-issue-test.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+import PouchTestHelper from '../helpers/pouch-test-helper';
+
+var App;
+
+describe('Acceptance: Create issue', function() {
+  beforeEach(function(done) {
+    App = startApp();
+
+    var currentTest = this.currentTest;
+
+    andThen(function() {
+      PouchTestHelper.setup(App, currentTest.title);
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    Ember.run(App, 'destroy');
+    PouchTestHelper.teardown(done);
+  });
+
+  it('lets the user create a new issue', function(done) {
+    visit('/');
+
+    click('button:contains("New issue")');
+    fillIn('input[name="title"]', 'Bananas are better');
+    click('button:contains("Done")');
+
+    visit('/');
+
+    andThen(function() {
+      expect(find('li:contains("Bananas are better")')).to.have.length(1);
+      done();
+    });
+  });
+});
