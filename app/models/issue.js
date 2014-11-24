fb4664e8c3d0c699e53ea51360b8d7a3af88ed42
Buck Doyle <b@chromatin.ca>
2014-11-24 00:35:49 -0500

    Add issue number

--- app/models/issue.js
+++ app/models/issue.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 var Issue = DS.Model.extend({
   title: DS.attr("string"),
+  number: DS.attr('number'),
   features: DS.hasMany('feature'),
 
   rev: DS.attr("string")
