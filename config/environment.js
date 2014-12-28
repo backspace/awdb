f281545012abab3cb985dc1d9a690b90c853602a
Buck Doyle <b@chromatin.ca>
2014-12-27 23:00:01 -0500

    Turn down logging

--- config/environment.js
+++ config/environment.js
@@ -27,10 +27,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     ENV.APP.databaseName = 'http://127.0.0.1:5984/awdb-development';
   }
