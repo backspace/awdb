fb953299372efef80f16f92721c2db1011890071
Buck Doyle <b@chromatin.ca>
2014-12-28 18:56:53 -0500

    Properly inflect distribute button

---
+++ app/helpers/plural-suffix.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+// Taken from https://coderwall.com/p/ryo_3w/add-the-pluralize-helper-in-ember-handlebars-templates
+
+export function pluralSuffix(number, options) {
+    var phraseMatch = ( options.hash.phrase || '{|s}' ).match( /(.*?)\{(.*?)\|(.*?)\}/ );
+    Ember.assert( 'The optional "phrase" hash for {{plural-suffix}} should be formatted as <phrase to pluralize>{<singular ending>|<plural ending>}', phraseMatch );
+    var word = phraseMatch[ 1 ],
+        singular = word + phraseMatch[ 2 ],
+        plural = word + phraseMatch[ 3 ];
+    return number === 1 ? singular : plural;
+}
+
+export default Ember.Handlebars.makeBoundHelper(pluralSuffix);
