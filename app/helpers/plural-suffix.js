0ec04ff5437ab602d8d1ac9bfe4cda4587675fce
Buck Doyle <b@chromatin.ca>
2015-01-06 13:23:52 -0500

    Change plural suffix helper to use square brackets

--- app/helpers/plural-suffix.js
+++ app/helpers/plural-suffix.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 // Taken from https://coderwall.com/p/ryo_3w/add-the-pluralize-helper-in-ember-handlebars-templates
 
 export function pluralSuffix(number, options) {
-    var phraseMatch = ( options.hash.phrase || '{|s}' ).match( /(.*?)\{(.*?)\|(.*?)\}/ );
-    Ember.assert( 'The optional "phrase" hash for {{plural-suffix}} should be formatted as <phrase to pluralize>{<singular ending>|<plural ending>}', phraseMatch );
+    var phraseMatch = ( options.hash.phrase || '[|s]' ).match( /(.*?)\[(.*?)\|(.*?)\]/ );
+    Ember.assert( 'The optional "phrase" hash for {{plural-suffix}} should be formatted as <phrase to pluralize>[<singular ending>|<plural ending>]', phraseMatch );
     var word = phraseMatch[ 1 ],
         singular = word + phraseMatch[ 2 ],
         plural = word + phraseMatch[ 3 ];
