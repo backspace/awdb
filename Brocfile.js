16aace041a82971d424a143deab1e3716ceac774
Buck Doyle <b@chromatin.ca>
2015-01-29 21:39:46 -0500

    Update Ember CLI to 0.1.11
    Various other packages demanded to be included

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-  'foundation-sass': {
+  'ember-cli-foundation-sass': {
     'foundationJs': ['joyride']
   }
 });
